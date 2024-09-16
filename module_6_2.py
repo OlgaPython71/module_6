b256ea3f2b59ab2005a423ce1094f4f3fcc153b7
OlgaPython71 <kuznetsova-o@mail.ru>
2024-09-16 17:03:53 +0300

    Create module_6_2.py

---
+++ module_6_2.py
@@ -0,0 +1,24 @@
+class Vehicle:
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self, owner, __model, __engine_power, __color):
+        self.owner = owner
+        self.__model = __model
+        self.__engine_power = __engine_power
+        self.__color = __color
+
+    def get_model(self):
+        return f'Модель: {self.__model}'
+
+    def get_horsepower(self):
+        return f'Мощность двигателя: {self.__engine_power}'
+
+    def get_color(self):
+        return f'Цвет: {self.__color}'
+
+    def print_info(self):
+        print(self.get_model(), self.get_horsepower(), self.get_color(), f'Владелец: {self.owner}')
+
+
+
+class Sedan(Vehicle):
