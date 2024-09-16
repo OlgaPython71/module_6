59359f4dce2882e098c82614a37dea1758fb0a30
OlgaPython71 <kuznetsova-o@mail.ru>
2024-09-16 19:28:48 +0300

    Update module_6_2.py

--- module_6_2.py
+++ module_6_2.py
@@ -17,8 +17,28 @@ class Vehicle:
         return f'Цвет: {self.__color}'
 
     def print_info(self):
-        print(self.get_model(), self.get_horsepower(), self.get_color(), f'Владелец: {self.owner}')
+        print(self.get_model(),  self.get_horsepower(), self.get_color(), f'Владелец: {self.owner}')
 
+    def set_color(self, new_color):
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
+        else:
+            print(f'Нельзя сменить цвет на {new_color}')
 
 
 class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500, 'blue')
+
+# Изначальные свойства
+vehicle1.print_info()
+
+# Меняем свойства (в т.ч. вызывая методы)
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+# Проверяем что поменялось
+vehicle1.print_info()
